5ab831d9153c7571b76d4636cfe842aa2ff2997a
Suchit Deshmukh <56268987+suchitd11@users.noreply.github.com>
2022-06-22 23:37:42 +0530

    data update

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id : 1,
         title : "Disney+",
-        about : "A web application that lets you know about all and trending movies and tv series with their trailers.",
+        about : "A web application with the information of various movies and series. They are divided into separate sections like top rated movies & popular series which is based on rating sorted & according to trend respectively. Also, user can tap on any show or movie and he/she will be able to read the plot and also play the trailer!",
         tags : ["NextJS", "NodeJS", "TMDB API","Firebase"],
         demo : "https://disney-clone-ndmiypj8n-suchitd11.vercel.app/",
         github : "https://github.com/suchitd11/Disney-clone",
@@ -10,8 +10,17 @@ export const ProjectData = [
     },
     {
         id : 2,
+        title : "PokemonStats",
+        about : "This is the web application basically a dashboard where in user can know about the different pokemons. Simply, type any name of the pokemon and hit the enter button, then user can see some information regarding that particular pokemon.",
+        tags : ["ReactJS", "PokeAPI"],
+        demo : "https://pokemondashboard.netlify.app/",
+        github : "https://github.com/suchitd11/PokemonStats",
+        image : "https://user-images.githubusercontent.com/56268987/172547181-00a8cbc3-926b-4e52-8b8a-2ed8ed889eb0.png"
+    },
+    {
+        id : 3,
         title : "WeCare",
-        about : "Sample Fundraising Website with payment gateway implemented for test payments.",
+        about : "Simple Fundraising Website made using Bootstrap from which test payments can be accepted. This website was made for test and educational purposes only.",
         tags : ["HTML", "CSS", "Bootstrap", "Razorpay"],
         demo : "https://wecare1.netlify.app/",
         github : "https://github.com/suchitd11/WeCare-PaymentIntegration",
